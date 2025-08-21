22c956e3d3c9faa70cbcc821f54ab9c6de2b5d74
Pampamlela <pampamlela@gmail.com>
2025-08-21 10:50:54 +0200

    add : detail page and app

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:les_vagues/widgets/bottom_nav.dart';
+import 'package:les_vagues/widgets/top_nav.dart';
+
+class DetailSpotPage extends StatelessWidget {
+  const DetailSpotPage({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyTopNav(),
+      body: Container(
+        child: Column(
+          children: [
+            Image.network(
+              "https://upload.wikimedia.org/wikipedia/commons/0/0a/Surf_at_Hossegor.jpg",
+              fit: BoxFit.cover,
+            ),
+
+            const SizedBox(height: 16),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [Text('Ville, Pays'), Text('Nom du spot')],
+                ),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Text('Note (nb votes)'),
+                    Text('Date ajout spot'),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 16),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Difficulté : '),
+                    Text('Type de vague :'),
+                    Text('Saison'),
+                  ],
+                ),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('*****'),
+                    Text('Reef Break'),
+                    Text('Juin'),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 16),
+
+            Text('Géolocalisation'),
+            Image.network(
+              "https://upload.wikimedia.org/wikipedia/commons/0/0a/Surf_at_Hossegor.jpg",
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: MyBottomNav(currentIndex: 0, onTap: (index) {}),
+    );
+  }
+}
--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyBottomNav extends StatelessWidget {
-   
   final int currentIndex;
   final Function(int) onTap;
 
@@ -20,14 +19,32 @@ class MyBottomNav extends StatelessWidget {
       unselectedItemColor: Colors.black,
       showSelectedLabels: true,
       showUnselectedLabels: true,
-      onTap: onTap,
+      onTap: (index) {
+        // tu déclenches ton callback pour mettre à jour l’état
+        onTap(index);
+
+        // et tu peux aussi gérer la navigation ici
+        switch (index) {
+          case 0:
+            Navigator.pushNamed(context, '/');
+            break;
+          case 1:
+            Navigator.pushNamed(context, '/search');
+            break;
+          case 2:
+            Navigator.pushNamed(context, '/add');
+            break;
+          case 3:
+            Navigator.pushNamed(context, '/profile');
+            break;
+        }
+      },
       items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home),label:'Accueil'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label:'Rechercher'),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label:'Ajouter'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label:'Profil'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Rechercher'),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Ajouter'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
       ],
     );
-    //throw UnimplementedError();
   }
 }
